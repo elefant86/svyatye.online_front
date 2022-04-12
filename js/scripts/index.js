b663ab039fe329526be424815ecbce1846a68cbc
Andrey <mailtiks@gmail.com>
2022-04-12 22:46:36 +0300

    Правки и адаптивность

--- js/scripts/index.js
+++ js/scripts/index.js
@@ -7,3 +7,12 @@ import iconSocialVK from '../../images/svg/social-vk.svg';
 import iconSocialYoutube from '../../images/svg/social-youtube.svg';
 import iconSocialDzen from '../../images/svg/social-dzen.svg';
 import iconSocialRutube from '../../images/svg/social-rutube.svg';
+
+var body = document.querySelector('body');
+var btnMenu = document.querySelector('.js-menu-open');
+var nav = document.querySelector('.nav');
+
+btnMenu.addEventListener("click", () => {
+    nav.classList.toggle("active");
+    body.classList.toggle("menuactive");
+});
